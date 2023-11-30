0a82103e0b82a48b317bc3ce5daec55804f69143
1-aminadab <4ethiochild@gmail.com>
2023-11-29 22:58:11 -0800

    remove some files

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,13 @@
-const {Client} = require('pg');
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT, } = require('../config/config');
+// const {Client} = require('pg');
+// const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT, } = require('../config/config');
 
-const client = new Client({
-  post : process.env.DB_PORT,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-});
-client.connect()
+// const client = new Client({
+//   post : process.env.DB_PORT,
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+// });
+// client.connect()
 
-module.exports = client;
+// module.exports = client;
